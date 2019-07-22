c8c78772e9a4e8770735f9963fab46d163de2f96
Francine Altman <francinekline@gmail.com>
2019-07-22 16:07:00 -0400

    test route working

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const { userRouter } = require('./routes/userRouter');
-const { questionRouter } = require('./routes/questionRouter');
+// const { userRouter } = require('./routes/userRouter');
+// const { questionRouter } = require('./routes/questionRouter');
 const { answerRouter } = require('./routes/answerRouter');
 
 const app = express();
@@ -15,8 +15,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/users', userRouter);
-app.get('/questions', questionRouter);
-app.get('/answers', answerRouter);
+// app.get('/users', userRouter);
+// app.get('/questions', questionRouter);
+app.use('/answers', answerRouter);
 
-app.listen(PORT, () => console.log(`Your tacos are being served on ${PORT}`));
+app.listen(PORT, () => console.log(`Your tacos are being served on port ${PORT}`));
