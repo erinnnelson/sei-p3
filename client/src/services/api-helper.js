29999d61b34e79f126e6de4656a7e2e01002411a
Erinn Nelson <erinn@Erinns-MacBook-Pro.local>
2019-07-25 17:32:59 -0400

    adjust BASE_URL in services for deployment

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://boiling-castle-93133.herokuapp.com/';
 const api = axios.create({
   baseURL: BASE_URL,
 });
